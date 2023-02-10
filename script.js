b657798932768b54c39efd005650d7e48097ee23
joshua-nelson <28989859+joshua-nelson@users.noreply.github.com>
2023-02-10 11:13:23 -0600

    Rework calculation logic to be more flexible

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const numberButtons = Array.from(document.querySelectorAll('.btn-number'));
 const operatorButtons = Array.from(document.querySelectorAll('.btn-operator'));
 const displayText = document.querySelector('.calculator__text');
-const operators = ['+', '*', '/', '-','.'];
+const operators = ['+', '*', '/', '-',];
 
-let displayValue = '';
-let secondValue = '';
-let firstValue = '';
-let operatorValue = null;
-let operatorPressed = false;
-let shouldResetDisplay = false;
+// let displayValue = '';
+// let secondValue = '';
+// let firstValue = '';
+// let operatorValue = null;
+// let operatorPressed = false;
+// let shouldResetDisplay = false;
 
+let calculationArray = [];
+let numberPressed = false;
+let operatorActive = false;
+let currentOperator;
+let previousOperator;
+let currentNumber;
 
+console.log(calculationArray.length);
 
 const operations = {
 
@@ -34,6 +41,8 @@ const operations = {
     }
 }
 
+let calc = {};
+
 
 function operate(operator, x , y) {
     
@@ -42,18 +51,21 @@ function operate(operator, x , y) {
     y = parseInt(y);
 
     let result = operations[operator](x,y);
-    operatorPressed = false;
-    
-    return result;
+    calculationArray = [];
+    updateDisplay(result);
     
 }
 
-function clear() {
-    updateDisplay(' ');
-    firstValue = '';
-    secondValue = '';
-    operatorValue = '';
-    operatorPressed = false;
+function buildCalculation(){
+    // If operator pressed and first value not set. Set first value
+  
+    if(operatorActive && !('first-value' in calc)) {
+        calc['first-value'] = displayText.textContent;
+        calc['operator'] = currentOperator;
+    } else if (operatorActive && 'first-value' in calc) {
+        calc['second-value'] = displayText.textContent;
+        operate(calc['operator'],calc['first-value'], calc['second-value'])
+    }
 }
 
 
@@ -63,35 +75,54 @@ function getDataValue(event){
 
 function updateDisplay(value){
    
-    displayText.textContent = value;
+    if(calculationArray.length == 0 && numberPressed == false) {
+        displayText.textContent = value;
+    } else if (numberPressed && !operatorActive) {
+        displayText.textContent += value;
+    } else if (!numberPressed  && operatorActive){
+        displayText.textContent += value;
+    } else if (numberPressed && operatorActive) {
+        displayText.textContent += value;
+    }
+    
+}
+
+function toggleActive(button) {
+    buttonClassList = button.target.classList;
+    
+    if(buttonClassList.contains('active')) {
+        buttonClassList.remove('active');
+        operatorActive = false;
+    } else {
+        buttonClassList.add('active')
+        operatorActive = true;
+        console.log(currentOperator);
+        buildCalculation();
+    }
     
 }
 
 function updateOperator(operator) {
-    if(operator == 'clear') clear();
- 
-    if(operatorPressed == true && operate != '=') {
-        let secondValue = displayText.textContent;
-        firstValue = operate(operatorValue,firstValue,secondValue);
-        updateDisplay(firstValue);
-        operatorPressed = false;
-        shouldResetDisplay = true;
-    } else {
-        firstValue = displayText.textContent;
-        operatorValue = operator;
-        operatorPressed = true;
-    }
-    
-    
+    numberPressed = false;
+    currentOperator = getDataValue(operator);
+    toggleActive(operator);
     
 }
 
-
+function updateNumber(number) {
+    currentNumber = getDataValue(number); 
+    updateDisplay(currentNumber);
+    numberPressed = true;
+   
+}
 
 numberButtons.forEach(button => {
-    button.addEventListener('click', (e) => updateDisplay(getDataValue(e)));
+    button.addEventListener('click', (e) => updateNumber(e));
 });
 
 operatorButtons.forEach(button => {
-    button.addEventListener('click', (e) => updateOperator(getDataValue(e)));
+    button.addEventListener('click', (e) => updateOperator(e));
 })
+
+
+
